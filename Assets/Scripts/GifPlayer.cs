cf8c92deb161c6ea98d54b07e37db7cf8b86fb09
Zihan Ye <zaeye1028@gmail.com>
2019-03-31 14:34:30 -0700

    sprite added

---
+++ Assets/Scripts/GifPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GifPlayer : MonoBehaviour
+{
+    public Texture[] frames;
+    float framesPerSecond = 10;
+
+    private void Update()
+    {
+        int index  = (int)(Time.time * framesPerSecond) % frames.Length;
+        GetComponent<Renderer>().material.mainTexture = frames[index];
+    }
+}
